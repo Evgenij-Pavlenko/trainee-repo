9dc281898151b086bb4b67316700d2ce6cf54179
Pavlenko, Ievgenii <Ievgenii.Pavlenko@adesso.de>
2021-10-05 15:25:42 +0200

    flyway-test-in

---
+++ flyway_spring_demo/src/main/java/com/example/flyway_spring_demo/FlywaySpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.flyway_spring_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywaySpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywaySpringDemoApplication.class, args);
+    }
+
+}
---
+++ flyway_spring_demo/src/test/java/com/example/flyway_spring_demo/FlywaySpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flyway_spring_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywaySpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
