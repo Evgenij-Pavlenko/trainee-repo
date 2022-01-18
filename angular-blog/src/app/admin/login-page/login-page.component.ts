35756af0db091ffd0988d68ce7d96f53a669253e
Pavlenko, Ievgenii <Ievgenii.Pavlenko@adesso.de>
2022-01-18 14:23:54 +0100

    add Autoriesed service

--- angular-blog/src/app/admin/login-page/login-page.component.ts
+++ angular-blog/src/app/admin/login-page/login-page.component.ts
@@ -1,43 +1,42 @@
-import {Component, OnInit} from '@angular/core'
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {User} from '../../shared/interfaces';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../../shared/interfaces';
+import { AuthService } from '../shared/service/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
+  form: FormGroup;
 
-  form: FormGroup
-
-  constructor() {
-  }
+  constructor(private auth: AuthService, private router: Router) {}
 
   ngOnInit() {
     this.form = new FormGroup({
-      email: new FormControl(null, [
-        Validators.required,
-        Validators.email
-      ]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [
         Validators.required,
-        Validators.minLength(6)
-      ])
-    })
+        Validators.minLength(6),
+      ]),
+    });
   }
 
   submit() {
     if (this.form.invalid) {
-      return
+      return;
     }
 
     const user: User = {
       email: this.form.value.email,
-      password: this.form.value.password
-    }
+      password: this.form.value.password,
+    };
 
-
+    this.auth.ligin(user).subscribe(() => {
+      this.form.reset();
+      this.router.navigate(['/admin', 'dashboard']);
+    });
   }
 }
-
