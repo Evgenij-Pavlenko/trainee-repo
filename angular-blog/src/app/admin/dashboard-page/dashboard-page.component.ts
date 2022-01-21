d9a635c87271db057d2793222feaaba7df4a882a
Pavlenko, Ievgenii <Ievgenii.Pavlenko@adesso.de>
2022-01-21 14:59:46 +0100

    delete post functional

--- angular-blog/src/app/admin/dashboard-page/dashboard-page.component.ts
+++ angular-blog/src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,36 +1,39 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {PostsService} from '../../shared/posts.service';
-import {Post} from '../../shared/interfaces';
-import {Subscription} from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PostsService } from '../../shared/posts.service';
+import { Post } from '../../shared/interfaces';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
-  styleUrls: ['./dashboard-page.component.scss']
+  styleUrls: ['./dashboard-page.component.scss'],
 })
 export class DashboardPageComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  pSub: Subscription;
+  dSub: Subscription;
+  searchStr = '';
 
-  posts: Post[] = []
-  pSub: Subscription
-  searchStr = ''
-
-  constructor(private postsService: PostsService) {
-  }
+  constructor(private postsService: PostsService) {}
 
   ngOnInit() {
-    this.pSub = this.postsService.getAll().subscribe(posts => {
-      this.posts = posts
-    })
+    this.pSub = this.postsService.getAll().subscribe((posts) => {
+      this.posts = posts;
+    });
   }
 
   remove(id: string) {
-
+    this.dSub = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter((post) => post.id != id);
+    });
   }
 
   ngOnDestroy() {
     if (this.pSub) {
-      this.pSub.unsubscribe()
+      this.pSub.unsubscribe();
+    }
+    if (this.dSub) {
+      this.dSub.unsubscribe;
     }
   }
-
 }
