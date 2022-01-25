7970142ceb6da5bc89fc9e2a77d4a575fff34672
Pavlenko, Ievgenii <Ievgenii.Pavlenko@adesso.de>
2022-01-25 16:19:28 +0100

    add a PWA

--- angular-blog/src/app/app.module.ts
+++ angular-blog/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { SharedModule } from './shared/shared.module';
 import { AuthInterceptor } from './shared/auth.interceptor';
 import { registerLocaleData } from '@angular/common';
 import deLocale from '@angular/common/locales/de';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 registerLocaleData(deLocale, 'de');
 
@@ -29,7 +31,17 @@ const INTERCEPTOR_PROVIDER: Provider = {
     PostPageComponent,
     PostComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   providers: [INTERCEPTOR_PROVIDER],
   bootstrap: [AppComponent],
 })
