e32d9d8183b359daa27eafac0c92320b316fcc48
Pavlenko, Ievgenii <Ievgenii.Pavlenko@adesso.de>
2022-01-25 15:46:07 +0100

    post display

--- angular-blog/src/app/post-page/post-page.component.ts
+++ angular-blog/src/app/post-page/post-page.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable, Subscription, switchMap } from 'rxjs';
+import { Post } from '../shared/interfaces';
+import { PostsService } from '../shared/services/posts.service';
 
 @Component({
   selector: 'app-post-page',
   templateUrl: './post-page.component.html',
-  styleUrls: ['./post-page.component.scss']
+  styleUrls: ['./post-page.component.scss'],
 })
 export class PostPageComponent implements OnInit {
+  post$: Observable<Post>;
+  pSub: Subscription;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService
+  ) {}
 
   ngOnInit() {
+    this.post$ = this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.postsService.getById(params['id']);
+      })
+    );
   }
-
 }
